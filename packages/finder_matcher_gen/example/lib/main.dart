f87a84a163ff337873e8cb4238ec24ca1ae42530
JasperEssien2 <jasperessien2@gmail.com>
2022-12-09 19:39:20 +0100

    fixes ongoing

--- packages/finder_matcher_gen/example/lib/main.dart
+++ packages/finder_matcher_gen/example/lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:finder_matcher_annotation/finder_matcher_annotation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,15 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
